6dc48ce40ce20bf99cccb760d9c3caf097e3e2cc
evgnep <enji81@gmail.com>
2020-03-08 11:34:18 +0300

    Сделана интернационализация csv-файла с вопросами и ответами

--- home1/src/main/java/ru/otus/home1/dao/QuestionDaoCSV.java
+++ home1/src/main/java/ru/otus/home1/dao/QuestionDaoCSV.java
@@ -11,18 +11,19 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Читает вопросы из CSV-файла из ресурсов
  * Формат файла:
  * первая строка (заголовок) - игнорируется
  * все остальные строки - вопросы
- *
+ * <p>
  * Первый столбец - сам вопрос
  * Второй столбец - фиксированный ответ
  * Третий столбец - признак правильного варианта (true/false)
  * Четвертый столбец - описание варианта и т.п.
- *
+ * <p>
  * Пример:
  * Вопрос;Ответ;Вариант 1 верный;Вариант 1
  * Автор Java;Гослинг
@@ -32,25 +33,9 @@ import java.util.List;
 public class QuestionDaoCSV implements QuestionDao {
     private final List<Question> questions = new ArrayList<>();
 
-    private Question parse(CSVRecord record) {
-        var size = record.size();
-
-        var question = new Question();
-        question.setText(record.get(0));
-        if (size < 3 || record.get(2).isEmpty()) {
-            question.setAnswer(record.get(1));
-        }
-        else {
-            for (int i = 2; i < size && !record.get(i).isEmpty(); i += 2) {
-                question.addChoice(record.get(i + 1), Boolean.parseBoolean(record.get(i)));
-            }
-        }
-
-        return question;
-    }
-
-    public QuestionDaoCSV(@Value("${application.questions}") String csvResourceName) throws IOException {
-        try (var stream = getClass().getResourceAsStream(csvResourceName)) {
+    public QuestionDaoCSV(@Value("${application.questions}") String csvResourceName, Locale locale) throws IOException {
+        String name = csvResourceName + (locale == null ? "" : "_" + locale.toString()) + ".csv";
+        try (var stream = getClass().getResourceAsStream(name)) {
             boolean first = true;
             for (var record : CSVFormat.EXCEL.withDelimiter(';').parse(new InputStreamReader(stream, "windows-1251"))) {
                 if (first) {
@@ -65,5 +50,21 @@ public class QuestionDaoCSV implements QuestionDao {
     @Override
     public List<Question> getQuestions() {
         return Collections.unmodifiableList(questions);
+    }
+
+    private Question parse(CSVRecord record) {
+        var size = record.size();
+
+        var question = new Question();
+        question.setText(record.get(0));
+        if (size < 3 || record.get(2).isEmpty()) {
+            question.setAnswer(record.get(1));
+        } else {
+            for (int i = 2; i < size && !record.get(i).isEmpty(); i += 2) {
+                question.addChoice(record.get(i + 1), Boolean.parseBoolean(record.get(i)));
+            }
+        }
+
+        return question;
     }
 }
