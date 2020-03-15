83859fd5b9c3129be6e5ec1328f2992c1089e8da
evgnep <enji81@gmail.com>
2020-03-15 22:10:48 +0300

    Завершен переход на spring-boot, сделан баннер

--- home1/src/main/java/ru/otus/home1/Main.java
+++ home1/src/main/java/ru/otus/home1/Main.java
@@ -1,28 +1,26 @@
 package ru.otus.home1;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 import ru.otus.home1.service.RunnerService;
+
+import java.util.Locale;
 
 @SpringBootApplication
 @EnableConfigurationProperties
 public class Main {
     public static void main(String[] args) {
-        try (var context = new AnnotationConfigApplicationContext()) {
-            context.register(Main.class);
-            context.refresh();
-
+        try (var context = SpringApplication.run(Main.class)) {
             var runnerService = context.getBean(RunnerService.class);
             runnerService.run();
         }
     }
 
-//    @Bean
-//    public MessageSource messageSource() {
-//        var ms = new ResourceBundleMessageSource();
-//        ms.setBasename("messages");
-//        ms.setDefaultEncoding("UTF-8");
-//        return ms;
-//    }
+    @Bean
+    public Locale locale(@Value("${application.locale}") Locale locale) {
+        return locale;
+    }
 }
--- home1/src/main/java/ru/otus/home1/dao/QuestionDaoCSV.java
+++ home1/src/main/java/ru/otus/home1/dao/QuestionDaoCSV.java
@@ -2,8 +2,8 @@ package ru.otus.home1.dao;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
-import ru.otus.home1.Properties;
 import ru.otus.home1.domain.Question;
 
 import java.io.IOException;
@@ -11,6 +11,7 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Читает вопросы из CSV-файла из ресурсов
@@ -32,8 +33,8 @@ import java.util.List;
 public class QuestionDaoCSV implements QuestionDao {
     private final List<Question> questions = new ArrayList<>();
 
-    public QuestionDaoCSV(Properties properties) throws IOException {
-        String name = properties.getQuestions() + (properties.getLocale() == null ? "" : "_" + properties.getLocale().toString()) + ".csv";
+    public QuestionDaoCSV(@Value("${application.questions}") String csvResourceName, Locale locale) throws IOException {
+        String name = csvResourceName + (locale == null ? "" : "_" + locale.toString()) + ".csv";
         try (var stream = getClass().getResourceAsStream(name)) {
             boolean first = true;
             for (var record : CSVFormat.EXCEL.withDelimiter(';').parse(new InputStreamReader(stream, "windows-1251"))) {
